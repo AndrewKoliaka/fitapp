3d1e96154b8ae7a36b4190fe28aa95554c54b920
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-02-15 16:56:07 +0200

    added basic adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,18 @@ window.onload = function(){
 	var foodCarousel = new Carousel('time_carousel_el', 'el_desc');
 	appCarousel.start();
 	foodCarousel.start();
+
+	function menuToggle(){
+		var toggleBtn = $('#menu-trigger'),
+			toggleMenu = $('.site-nav ul');
+		toggleBtn.click(function(){
+			toggleMenu.slideToggle(200);
+		});
+		$(window).resize(function() {
+			if ($(window).width() > 992) {
+				toggleMenu.removeAttr('style');
+			}
+		})
+	}
+	menuToggle();
 }
